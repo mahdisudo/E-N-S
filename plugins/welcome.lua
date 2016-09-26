3d5bef5a50ab9c64332284627ae72d34d7096c14
mahdisudo <mehdisudo@gmail.com>
2016-09-26 15:21:39 +0330

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -58,7 +58,7 @@ local function description_rules(msg, nama)
          rules = data[tostring(msg.to.id)]["rules"]
          rules = "\nRules :\n"..rules.."\n"
       end
-      local sambutan = "hi "..nama.." 🔰welcome to ["..string.gsub(msg.to.print_name, "_", " ").."]"
+      local sambutan = "درود\n "..nama.." خوش اومدی به ⚓️"..string.gsub(msg.to.print_name, "_", " ").."🚀"
       local text = sambutan..about..rules.."\n"
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
@@ -88,7 +88,7 @@ local function run(msg, matches)
       description_rules(msg, nama)
    elseif matches[1] == "chat_del_user" then
        local bye_name = "@"..msg.action.user.username
-       return '🔰goodbye '..bye_name..'☻' 
+       return 'خدانگهدار '..bye_name..'🚀' 
    end
 end
 
